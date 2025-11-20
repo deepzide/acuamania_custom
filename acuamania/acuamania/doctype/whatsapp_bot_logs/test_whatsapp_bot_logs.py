ee5ff099399e6e1fbf1a92f709dc3f3a69b0f2b1
cegomezpy <cegomezpy@gmail.com>
2025-11-20 15:51:55 +0000

    feat: add-new-doctypes

---
+++ acuamania/acuamania/doctype/whatsapp_bot_logs/test_whatsapp_bot_logs.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, cegomezpy and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestWhatsAppBotLogs(FrappeTestCase):
+	pass
