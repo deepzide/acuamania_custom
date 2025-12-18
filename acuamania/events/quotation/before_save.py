4c195d6819592c8acf47ca17e1d575daad2b1493
cegomezpy <cegomezpy@gmail.com>
2025-12-18 03:30:07 +0000

    fix: remove non used scripts

--- acuamania/events/quotation/before_save.py
+++ acuamania/events/quotation/before_save.py
@@ -1,7 +1,5 @@
 from acuamania.acuamania.promo_engine.engine import apply_selected_promotion
-from acuamania.acuamania.promo_engine.set_default_group_promo import set_default_group_promo
 
 
 def before_save(doc, method=None):
-    set_default_group_promo(doc)
     apply_selected_promotion(doc)
